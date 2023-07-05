21a34f10815f03d3b88d500e94918478c33c2d97
Kunal Singh <95497165+drallkunal@users.noreply.github.com>
2023-07-05 20:19:54 +0530

    Update rockPaperScissorsGame.cpp

--- rockPaperScissorsGame.cpp
+++ rockPaperScissorsGame.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <ctime>
 
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner(char player, char computer);
+
+int main() {
+
+	char player;
+	char computer;
+
+	player = getUserChoice();
+	std::cout << "Your choice: ";
+	showChoice(player);
+
+	computer = getComputerChoice();
+	std::cout << "Computer's choice: ";
+	showChoice(computer);
+
+	chooseWinner(player, computer);
+ 
+    return 0;
+}
