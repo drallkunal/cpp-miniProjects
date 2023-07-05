c0c4826b599c85229c701a320cc3db9ebc2eafcf
Kunal Singh <95497165+drallkunal@users.noreply.github.com>
2023-07-05 20:20:27 +0530

    Update rockPaperScissorsGame.cpp

--- rockPaperScissorsGame.cpp
+++ rockPaperScissorsGame.cpp
@@ -23,3 +23,19 @@ int main() {
  
     return 0;
 }
+char getUserChoice(){
+
+	char player;
+	std::cout << "Rock-Paper-Scissors Game!\n";
+
+	do{
+		std::cout << "Choose one of the following\n";
+		std::cout << "*************************\n";
+		std::cout << "'r' for rock\n";
+		std::cout << "'p' for paper\n";
+		std::cout << "'s' for scissors\n";
+		std::cin >> player;
+	}while(player != 'r' && player != 'p' && player != 's');
+
+	return player;
+}
